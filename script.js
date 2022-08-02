14156f82bf6e549780cff71ced11aaffaf2eafdb
Vitor Hugo <93061834+01Vitorhugo@users.noreply.github.com>
2022-08-02 18:03:48 -0300

    Add files via upload

---
+++ script.js
@@ -0,0 +1,16 @@
+
+var email;
+var senha;
+
+
+function login(){
+
+email = document.form.email.value;
+senha = document.form.senha.value;
+
+console.log('E-mail: \n'+email+ '\nSenha: \n'+senha);
+
+window.alert('Login Aceito !');
+
+
+}
